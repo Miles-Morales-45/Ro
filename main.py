f2e351e18569c8f9bec6782f5d88b92ce27c6663
Miels-Morales-45 <bililia555@mail.ru>
2019-10-14 20:31:51 +1000

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,36 @@ class Lexer:
         print('Lexer error'), msg
         sys.exit
 
-    def getf(self):
+    def getc(self):
         self.ch = sys.stdin.read(1)
 
     def next_token(self):
         self.value = None
-        self.sim = None
+        self.sym = None
+        while self.sym == None:
+            if len(self.ch) == 0:
+                self.sym == Lexer.EOF
+            elif self.ch.isspace():
+                self.getc()
+            elif self.ch.isdigit():
+                intval = 0
+                while self.ch.isdigit():
+                    intval = intval * 10 + int(self.ch)
+                    self.getc()
+                self.value = intval
+                self.sym = Lexer.NUM
+
+            elif self.sym.isalpha():
+                ident = ' '
+                while self.ch.isalpha():
+                    ident = ident + self.ch.lower()
+                    self.getc
+                if ident in Lexer.WORDS:
+                    self.sym = Lexer.WORDS[ident]
+                elif len(ident) == 1:
+                    self.sym == Lexer.ID
+                    self.value = ord(ident) - ord("a")
+                else:
+                    self.error("Unknow identifier")
+            else:
+                self.error("Unxpected symbol: " + self.ch)
